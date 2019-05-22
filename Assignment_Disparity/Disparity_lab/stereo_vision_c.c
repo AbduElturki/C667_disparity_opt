5112f054eb162c38ede62adc6d6e11bf385ab850
Abdu Elturki <abdussalam@elturki.com>
2019-05-22 13:31:17 +0100

    0.0278 C, 0.0374

--- Assignment_Disparity/Disparity_lab/stereo_vision_c.c
+++ Assignment_Disparity/Disparity_lab/stereo_vision_c.c
@@ -11,8 +11,9 @@ void stereo_vision_c(unsigned char * restrict L, unsigned char * restrict R, uns
     int i,j;
     int Sum, Distance,Minimize;
     int ones = 0x01010101;
-    unsigned char left_buf[32];
-    unsigned char right_buf[32];
+    long long mask = 0xFFFFFFFFFF;
+    long long left_buf[5];
+    long long right_buf[5];
     int flag;
     int s;
 
@@ -23,17 +24,17 @@ void stereo_vision_c(unsigned char * restrict L, unsigned char * restrict R, uns
     #pragma MUST_ITERATE(Height-5,,2)
     for (i=(Height-1)-Radius;i>=Radius;i--)
     {
+#pragma MUST_ITERATE(Width-5,,2)
         for (j=(Width-1)-Radius;j>=Radius;j--)
         {
             Minimize=100000;
             Sum = 100000;
 
-            memcpy(&left_buf[0],&L[(i-2)*Width+j-Radius],5);
-            memcpy(&left_buf[5],&L[(i-1)*Width+j-Radius],5);
-            memcpy(&left_buf[10],&L[(i)*Width+j-Radius],5);
-            memcpy(&left_buf[15],&L[(i+1)*Width+j-Radius],5);
-            memcpy(&left_buf[20],&L[(i+2)*Width+j-Radius],5);
-            memcpy(&left_buf[25],&L[(i+3)*Width+j-Radius],5);
+            left_buf[0] = _mem8_const(&L[(i-2)*Width+j-Radius]) & mask;
+            left_buf[1] = _mem8_const(&L[(i-1)*Width+j-Radius]) & mask;
+            left_buf[2] = _mem8_const(&L[(i)*Width+j-Radius]) & mask;
+            left_buf[3] = _mem8_const(&L[(i+1)*Width+j-Radius]) & mask;
+            left_buf[4] = _mem8_const(&L[(i+2)*Width+j-Radius]) & mask;
 
             flag = (j-Radius-Search_Range < 0);
             int search = !flag * Search_Range + flag * (j-Radius+1);
@@ -41,23 +42,26 @@ void stereo_vision_c(unsigned char * restrict L, unsigned char * restrict R, uns
             #pragma MUST_ITERATE(2,,2)
             for (s=0; s<search; s++)
             {
-                memcpy(&right_buf[0],&R[(i-2)*Width+j-Radius-s],5);
-                memcpy(&right_buf[5],&R[(i-1)*Width+j-Radius-s],5);
-                memcpy(&right_buf[10],&R[(i)*Width+j-Radius-s],5);
-                memcpy(&right_buf[15],&R[(i+1)*Width+j-Radius-s],5);
-                memcpy(&right_buf[20],&R[(i+2)*Width+j-Radius-s],5);
+                right_buf[0] = _mem8_const(&R[(i-2)*Width+j-Radius-s]) & mask;
+                right_buf[1] = _mem8_const(&R[(i-1)*Width+j-Radius-s]) & mask;
+                right_buf[2] = _mem8_const(&R[(i)*Width+j-Radius-s]) & mask;
+                right_buf[3] = _mem8_const(&R[(i+1)*Width+j-Radius-s]) & mask;
+                right_buf[4] = _mem8_const(&R[(i+2)*Width+j-Radius-s]) & mask;
 
                 flag = (Sum<Minimize);
                 Distance = ((!(flag)) * Distance) + (flag * (s-1));
                 Minimize = ((!(flag)) * Minimize) + (flag * Sum);
 
-                Sum = _dotpu4(_subabs4(_hill(_amem8(&left_buf[0])),_hill(_amem8(&right_buf[0]))),ones);
-                Sum += _dotpu4(_subabs4(_loll(_amem8(&left_buf[0])),_loll(_amem8(&right_buf[0]))),ones);
-                Sum += _dotpu4(_subabs4(_hill(_amem8(&left_buf[8])),_hill(_amem8(&right_buf[8]))),ones);
-                Sum += _dotpu4(_subabs4(_loll(_amem8(&left_buf[8])),_loll(_amem8(&right_buf[8]))),ones);
-                Sum += _dotpu4(_subabs4(_hill(_amem8(&left_buf[16])),_hill(_amem8(&right_buf[16]))),ones);
-                Sum += _dotpu4(_subabs4(_loll(_amem8(&left_buf[16])),_loll(_amem8(&right_buf[16]))),ones);
-                Sum += _dotpu4(_subabs4(_amem4(&left_buf[24]),_amem4(&right_buf[24])),ones);
+                Sum = _dotpu4(_subabs4(_hill(left_buf[0]),_hill(right_buf[0])),ones);
+                Sum += _dotpu4(_subabs4(_loll(left_buf[0]),_loll(right_buf[0])),ones);
+                Sum += _dotpu4(_subabs4(_hill(left_buf[1]),_hill(right_buf[1])),ones);
+                Sum += _dotpu4(_subabs4(_loll(left_buf[1]),_loll(right_buf[1])),ones);
+                Sum += _dotpu4(_subabs4(_hill(left_buf[2]),_hill(right_buf[2])),ones);
+                Sum += _dotpu4(_subabs4(_loll(left_buf[2]),_loll(right_buf[2])),ones);
+                Sum += _dotpu4(_subabs4(_hill(left_buf[3]),_hill(right_buf[3])),ones);
+                Sum += _dotpu4(_subabs4(_loll(left_buf[3]),_loll(right_buf[3])),ones);
+                Sum += _dotpu4(_subabs4(_hill(left_buf[4]),_hill(right_buf[4])),ones);
+                Sum += _dotpu4(_subabs4(_loll(left_buf[4]),_loll(right_buf[4])),ones);
 
             }
             flag = (Sum<Minimize);
